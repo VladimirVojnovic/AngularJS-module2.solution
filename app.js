c6e7f408e4ae4d15c92bc34885088dbd790672ef
Vladimir Vojnović <vladavoj@gmail.com>
2016-09-23 13:40:00 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -22,25 +22,27 @@ function ToBuyController(ShoppingListCheckOffService, $scope) {
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService','$scope'];
 function AlreadyBoughtController(ShoppingListCheckOffService, $scope) {
   $scope.boughtList = ShoppingListCheckOffService.boughtList;
-
+  $scope.isEmptyBuyList = function () {
+			return ShoppingListCheckOffService.toBuyList.length ? false : true;
+	};
 
 } //AlreadyBoughtController function
 
 function ShoppingListCheckOffService(){
 
 // Initial list of items to buy
-  var toBuy = [{name:"cookies", quantity:"10 bags"},
-                    {name:"chocholate", quantity:"3 packs"},
-                    {name:"milk", quantity:"6 bottles"},
-                    {name:"Coca Cola", quantity:"4 bottles"},
-                    {name:"flour", quantity:"2 kg"},
-                    {name:"oil", quantity:"1 liters"},
-                    {name:"vinegar", quantity:"0,5 liter"},
-                    {name:"tomatoes", quantity:"1 kg"},
-                    {name:"potatoes", quantity:"2 kg"},
-                    {name:"mineral water", quantity:"5 liters"},
-                    {name:"red wine", quantity:"2 bottles"},
-                    {name:"beer", quantity:"6 cans"}];
+  var toBuy = [{name:'cookies', quantity:'10 bags'},
+                    {name:'chocholate', quantity:'3 packs'},
+                    {name:'milk', quantity:'6 bottles'},
+                    {name:'Coca Cola', quantity:'4 bottles'},
+                    {name:'flour', quantity:'2 kg'},
+                    {name:'oil', quantity:'1 liters'},
+                    {name:'vinegar', quantity:'0,5 liter'},
+                    {name:'tomatoes', quantity:'1 kg'},
+                    {name:'potatoes', quantity:'2 kg'},
+                    {name:'mineral water', quantity:'5 liters'},
+                    {name:'red wine', quantity:'2 bottles'},
+                    {name:'beer', quantity:'6 cans'}];
 // Initial list of bought items
   var bought = [];
 
